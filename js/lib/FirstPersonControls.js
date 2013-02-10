acbb2e3ed2e5c07e9a63437f4795660322e421ed
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-02-09 18:24:07 -0800

    initial commit of actual files

---
+++ js/lib/FirstPersonControls.js
@@ -0,0 +1,122 @@
+function FirstPersonControls(element) { //element should be a canvas
+  this.element = element;
+  this.movementYTotal = this.movementXTotal = 0;
+}
+
+FirstPersonControls.prototype.register = function() {
+  var havePointerLock = 'pointerLockElement' in document ||
+                        'mozPointerLockElement' in document ||
+                        'webkitPointerLockElement' in document;
+  if(!havePointerLock) alert("no pointer lock!");
+
+  var element = this.element;
+  element.requestPointerLock = element.requestPointerLock ||
+                               element.mozRequestPointerLock ||
+                               element.webkitRequestPointerLock;
+
+  document.exitPointerLock = document.exitPointerLock ||
+                             document.mozExitPointerLock ||
+                             document.webkitExitPointerLock;
+
+  this.onClick = function() {
+    // Ask the browser to lock the pointer
+    element.requestPointerLock();
+  };
+
+  element.addEventListener('click', this.onClick, false);
+
+  var moveCallback = function(e) {
+    var movementX = e.movementX ||
+                    e.mozMovementX          ||
+                    e.webkitMovementX       ||
+                    0,
+        movementY = e.movementY ||
+                    e.mozMovementY      ||
+                    e.webkitMovementY   ||
+                    0;
+
+    this.movementXTotal += movementX;
+    this.movementYTotal += movementY;
+
+  }.bind(this);
+
+  var keyState = this.keyState = {};
+  function keyDown(evt) {
+    keyState[String.fromCharCode(evt.keyCode)] = true;
+  };
+
+  function keyUp(evt) {
+    delete keyState[String.fromCharCode(evt.keyCode)];
+  };
+
+  function lockChangeCallback(evt) {
+    var requestedElement = element;
+    if (document.pointerLockElement === requestedElement ||
+        document.mozPointerLockElement === requestedElement ||
+        document.webkitPointerLockElement === requestedElement) {
+      console.log("got lock");
+      // Pointer was just locked - register fps event listeners
+      document.addEventListener("mousemove", moveCallback, false);
+
+      document.addEventListener('keydown', keyDown, false);
+      document.addEventListener('keyup', keyUp, false);
+    } else {
+      console.log("released lock");
+      // Pointer was just unlocked - unregister
+      document.removeEventListener("mousemove", moveCallback, false);
+
+      document.removeEventListener("keydown", keyDown, false);
+      document.removeEventListener("keyup", keyUp, false);
+    }
+  };
+
+  document.addEventListener('pointerlockchange', lockChangeCallback, false);
+  document.addEventListener('mozpointerlockchange', lockChangeCallback, false);
+  document.addEventListener('webkitpointerlockchange', lockChangeCallback, false);
+
+
+
+
+
+  gameworld.player.model.visible = false;
+};
+
+FirstPersonControls.prototype.unregister = function() {
+  document.exitPointerLock();
+  this.element.removeEventListener('click', this.onClick);
+  gameworld.player.model.visible = true;
+};
+
+FirstPersonControls.prototype.update = function() {
+  var d_azimuth = ( THREE.Math.mapLinear(this.movementXTotal, 0, renderer.domElement.width/2, 0, this.fovx() / 2) ) * Math.PI / 180;
+  var d_altitude = ( THREE.Math.mapLinear(this.movementYTotal, 0, renderer.domElement.height/2, 0, -camera.fov/2 ) ) * Math.PI / 180;
+  this.movementXTotal = this.movementYTotal = 0;
+
+  gameworld.player.moveHeading(d_azimuth, d_altitude);
+
+  var keyMap = {};
+  keyMap['W'] = new THREE.Vector2(1, 0);
+  keyMap['S'] = new THREE.Vector2(-1, 0);
+  keyMap['D'] = new THREE.Vector2(0, 1);
+  keyMap['A'] = new THREE.Vector2(0, -1);
+  var offset = new THREE.Vector2();
+  for(key in this.keyState) {
+    if(key in keyMap) { offset.addSelf(keyMap[key]); }
+  }
+  offset.normalize();
+  gameworld.player.setMoveDirection(offset.toBox2D());
+
+  //move camera to reflect new state
+  var pos2 = gameworld.player.position().toTHREE();
+
+  var camera_position = terrain.onTerrain( pos2 );
+  camera_position.y += gameworld.player.tall;
+
+  var heading = gameworld.player.heading();
+  camera.position.copy( camera_position );
+  camera.lookAt( camera_position.clone().addSelf( heading ) );
+};
+
+FirstPersonControls.prototype.fovx = function () {
+  return camera.fov * camera.aspect;
+}
---
+++ theplanet.js
@@ -0,0 +1,133 @@
+window.onFinish.push(function () {
+
+
+map = {
+  heightAt: function(pos) {
+              var noise1 = .5 * noise.noise(v.x, v.y, v.z);
+              var noise2 = .1 * noise.noise(v.x * 5, v.y * 5, v.z * 5);
+              var noise3 = .01 * noise.noise(v.x * 50, v.y * 50, v.z * 50);
+              return 8 + noise1 + noise2 + noise3;
+            },
+  onTerrain: function(pos) {
+               return pos.copy().setLength(this.heightAt(pos));
+             }
+};
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 1, 10000);
+  camera.position.set( 20, 20, 20 );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  controls = window.trackballControls = new THREE.TrackballControls(camera);
+  scene.add(new THREE.AxisHelper(10));
+
+
+  cube = new THREE.CubeGeometry(10, 10, 10, 100, 100, 100);
+  map.geometry = cube;
+
+  var noise = new ClassicalNoise();
+  cube.vertices.forEach(function (v) {
+    v.setLength(4);
+    var noise1 = .5 * noise.noise(v.x, v.y, v.z);
+    var noise2 = .1 * noise.noise(v.x * 5, v.y * 5, v.z * 5);
+    var noise3 = .01 * noise.noise(v.x * 50, v.y * 50, v.z * 50);
+    v.setLength(8 + noise1 + noise2 + noise3);
+  });
+  cube.verticesNeedUpdate = true;
+  cube.computeFaceNormals();
+  cube.computeVertexNormals();
+  cube.computeCentroids();
+  cube.computeTangents();
+  cube.normalsNeedUpdate = true;
+
+  // mesh = THREE.SceneUtils.createMultiMaterialObject(cube, [new THREE.MeshPhongMaterial({color: 0xff0000}), new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true})]);
+  mesh = THREE.SceneUtils.createMultiMaterialObject(cube, [new THREE.MeshPhongMaterial({color: 0xff0000})]);
+  scene.add(mesh);
+
+  avgLength = _.reduce(cube.vertices, function(memo, v) { return memo + v.length(); }, 0) / ( cube.vertices.length );
+  waterMesh = new THREE.Mesh(new THREE.SphereGeometry(avgLength, 50, 50), new THREE.MeshLambertMaterial({color: 0x84d4b4}));
+  scene.add(waterMesh);
+
+  map.waterHeight = avgLength;
+
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  scene.add(light);
+
+  //set up stats
+  stats = new Stats();
+  stats.setMode(0); // 0: fps, 1: ms
+
+  // Align top-left
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+
+  document.body.appendChild( stats.domElement );
+};
+
+// function faceFor(pos) {
+//   //1) find the face of the cube the pos is in
+//   //2) project the pos onto the face's plane, using a coordinate system to represent the 
+//   //3) 
+// 
+//   //or: face whose centroid is closest
+//   //
+//   map.geometry.faces.map(
+// 
+// }
+
+// function isWalkable(pos) {
+//   return map.geometry.
+// }
+
+//pos: Vector3 of unit length representing where it is on the sphere
+//forward: Vector3 of unit length representing where it's facing
+// function Entity(pos, forward) {
+//   this.pos = pos; //
+//   this.forward = forward;
+//   var left = new THREE.Vector3().cross(pos, forward);
+// 
+//   this.mesh = new THREE.Mesh(new THREE.CubeGeometry(.1, .1, .1), new THREE.MeshNormalMaterial());
+//   scene.add(this.mesh);
+// 
+//   this.update();
+// }
+// Entity.prototype.update = function() {
+//   this.mesh.position = map.onTerrain(this.pos);
+//   this.mesh.rotation = 
+// }
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function render() {
+  stats.begin();
+  requestAnimationFrame(render);
+  controls.update();
+
+  // waterMesh.geometry.vertices.forEach(function (v) {
+  //   v.setLength(avgLength * (.99 + Math.sin(v.x * v.y * v.z + (new Date()).getTime() / 1000) *.003 ) );
+  // });
+  // waterMesh.geometry.verticesNeedUpdate = true;
+  // waterMesh.geometry.computeFaceNormals();
+  // waterMesh.geometry.computeVertexNormals();
+  // waterMesh.geometry.normalsNeedUpdate = true;
+
+  renderer.render(scene, camera);
+  stats.end();
+}
+
+init();
+render();
+});
+
